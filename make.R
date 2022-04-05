6be094daaac37ecd79dbfe7d224d0a305cea0fe9
Danet <ahdanet@ilstu.edu>
2022-04-05 09:26:24 -0500

    Update report with complete exotic species dataset

--- make.R
+++ make.R
@@ -11,7 +11,7 @@ tar_visnetwork()
 
 tar_make(names = meeting_report)
 tar_make(names = biodiversity_facets_support)
-tar_make(names = c(mod_exo_comp_std))
+tar_make(names = c(mod_exo_comp_std, mod_exo_comp))
 
 
 source(file = "start_rmd.R")
