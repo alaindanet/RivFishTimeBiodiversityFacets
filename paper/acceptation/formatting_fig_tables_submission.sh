ecdb130ef71a3d1fec59110c6e9b650d74f7c06a
Alain Danet <alain.danet@mnhn.fr>
2023-11-29 09:40:35 +0000

    Formatting changes for NEE

--- paper/acceptation/formatting_fig_tables_submission.sh
+++ paper/acceptation/formatting_fig_tables_submission.sh
@@ -16,15 +16,15 @@ cp ../fig_paper/p_trends_supp.pdf extended_data/Danet_ED_Fig3.pdf
 cp ../fig_paper/clust_figure_na.pdf extended_data/Danet_ED_Fig4.pdf
 cp ../fig_paper/p_pred_hft.pdf extended_data/Danet_ED_Fig5.pdf
 cp ../fig_paper/p_drivers_ts_caracteristic.pdf extended_data/Danet_ED_Fig6.pdf
-cp ../fig_paper/p_effect_supp.pdf extended_data/Danet_ED_Fig7.pdf
-cp ../fig_paper/p_pca_riv_str.pdf extended_data/Danet_ED_Fig8.png
+cp ../fig_paper/p_pca_riv_str.pdf extended_data/Danet_ED_Fig7.pdf
+cp ../fig_paper/p_effect_supp.pdf extended_data/Danet_ED_Fig8.pdf
 cp ../fig_paper/tab_rand_extended_data_table1.jpg extended_data/Danet_ED_Table1.jpg
 
 # Supplementary Information
 cp ../supplementary_figures.pdf Danet_SI.pdf
 
 # Supplementary Data
-zip -r Danet_Supplementary_Data_1.zip ../fig_paper/SupplementaryMap
+zip -r Danet_Supplementary_Software_1.zip ../fig_paper/SupplementaryMap
 
 # Reporting summary
 cp nr-reporting-summary_filed.pdf Danet_RS.pdf
